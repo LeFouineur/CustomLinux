2c5abfdcbc90bfe11b0674f6aeb76c591fa9fee4
LeFouineur <lefouineur@gmail.com>
2018-08-12 21:11:10 +0200

    + Script de récupération des fichiers de personnalisation depuis le dépôt GitHub

---
+++ Scripts/Retrieve_files.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.bash_aliases
+wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.bashrc
+wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.nanorc
+wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.vimrc
