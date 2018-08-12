b7996cffcad6da8b8b8171b6d815f74c3f925f41
LeFouineur <lefouineur@gmail.com>
2018-08-12 21:15:02 +0200

    * Update of the URL to retrieve the files as a RAW format

--- Scripts/Retrieve_files.sh
+++ Scripts/Retrieve_files.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.bash_aliases
-wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.bashrc
-wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.nanorc
-wget https://github.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.vimrc
+wget https://raw.githubusercontent.com/LeFouineur/CustomLinux/master/Personnalisation/user/.bash_aliases
+wget https://raw.githubusercontent.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.bashrc
+wget https://raw.githubusercontent.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.nanorc
+wget https://raw.githubusercontent.com/LeFouineur/CustomLinux/blob/master/Personnalisation/user/.vimrc
